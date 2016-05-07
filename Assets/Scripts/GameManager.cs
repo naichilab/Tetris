2fbbc25496a712ba6be29a5e17bf0fa5d27bdafd
naichilab <naichilab@live.jp>
2016-05-07 15:17:27 +0900

    枠うごかした

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GameManager : SingletonMonoBehaviour<GameManager>
+{
+
+	public void Awake ()
+	{
+		if (this != Instance) {
+			Destroy (this);
+			return;
+		}
+
+		DontDestroyOnLoad (this.gameObject);
+	}
+
+}
